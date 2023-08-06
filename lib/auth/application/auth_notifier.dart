1f1f787a5cb93115f39e03304c61104bbfb45be4
ermjonertas <ermert.jonas@yahoo.de>
2023-08-06 09:10:04 +0200

    Writing the First AuthNotifier Method

--- lib/auth/application/auth_notifier.dart
+++ lib/auth/application/auth_notifier.dart
@@ -4,9 +4,20 @@ part 'auth_notifier.freeezed.dart';
 
 @freezed
 class AuthState with _$AuthState {
-    const AuthState.:();
+    const AuthState._();
     const factory AuthState.initial() = _Initial;
     const factory AuthState.unauthenticated() = _Unauthenticated;
     const factory AuthState.failure(AuthFailure failure) = _Failure;
     
+}
+
+class AuthNotifier extends StateNotifier<AuthState> {
+    final GithubAuthenticator _authenticator;
+
+    AuthNotifier(this._authenticator) : super(const AuthState.initial());
+
+    Future<void> checkAndUpdateAuthStatus() async {state = await _authenticator.isSignedIn())
+        ? const AuthState.authenticated()
+        : const AuthState.unauthenticated();
+    }
 }
