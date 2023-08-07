13aab514825d07eb573768e515c9c778a23112e9
ermjonertas <ermert.jonas@yahoo.de>
2023-08-07 15:17:14 +0200

    Creating a Splash Page

--- lib/splash/presentation/splash_page.dart
+++ lib/splash/presentation/splash_page.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 class SplashPage extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        return Container (
-            
-        );
+        return Scaffold (body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48),
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+                Icon(
+                   MdiIcons.github,
+                   size: 150, 
+                )
+                SizedBox(height: 16),
+                LinearProgressIndicator();
+            ]));
     }
 
 
