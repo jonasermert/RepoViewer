cd6b003ada7943d5c05a531beeeffd37cd522151
ermjonertas <ermert.jonas@yahoo.de>
2023-08-07 14:29:22 +0200

    Setting Up Presentation Layer Pages

---
+++ lib/starred_repos/presentation/starred_repos_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StarredResposPage extends StatelessWidget {
+    const StarredResposPage({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Container (
+            
+        );
+    }
+
+
+
+}
