b7a4c0720af65191b8982a663e8250f7f747a0cf
ermjonertas <ermert.jonas@yahoo.de>
2023-08-08 09:23:40 +0200

    Building the Sign-In Page UI

--- lib/auth/presentation/sign_in_page.dart
+++ lib/auth/presentation/sign_in_page.dart
@@ -6,6 +6,34 @@ class SignInPage extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return Scaffold (
+            body: SafeArea(
+                child: Center(
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 48),
+                    child: SingleChildScrollView(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                            const Icon(
+                                MdiIcons.github,
+                                size 150,
+                            ),
+                            const SizedBox(height: 16)
+                            Text('Welcome to Repo Viewer', style: Theme.of(context).textTheme.headline3, textAlign: TextAlign.center
+                            ),
+                            const SizeBox(height: 32),
+                            ElevatedButton(onPressed: () {},
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(Colors.green),
+                            ),
+                            child: text('Sign In'))
+                        ]
+                    )
+                    )
+                    )
+                )
+            )
             
         );
     }
