ebc6a685002430d3717af88f17a577ce90a43066
Jonas Ermert <ermert.jonas@yahoo.de>
2023-08-09 10:59:14 +0200

    Creating the GitHub Feature Folder Structure

--- lib/core/presentation/routes/app_router.dart
+++ lib/core/presentation/routes/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:repoviewer/auth/presentation/sign_in_pge.dart';
+import 'package:repoviewer/auth/presentation/sign_in_page.dart';
 import 'package:repoviewer/splash/presentation/splash_page.dart';
-import 'package:repoviewer/starred_repos/presentation/starred_repos.page.dart';
+import 'package:repoviewer/github/repos/starred_repos/presentation/starred_repos_page.dart';
+
+import '../../../auth/presentation/authorization_page.dart';
 
 @MaterialAutoRouter(routes:[
     MaterialRoute(page: SplashPage, initial: true),
